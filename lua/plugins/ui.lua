0420f5f08c13d2881f93db1c54a39dd17da1e871
Vladimir Mikhalenko <mikhalenko@acdlabs.by>
2026-06-04 08:25:33 +0100

    resume files/grep picker, custom explorer_add action in snacks.explorer for creating multiple file at once

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,13 @@
+local function resume_picker(command, opts)
+  local resume = require("snacks.picker.resume")
+
+  if resume.state[command] then
+    Snacks.picker.resume(command)
+  else
+    LazyVim.pick(command, opts)()
+  end
+end
+
 return {
   { "folke/noice.nvim", enabled = false },
 
@@ -78,11 +88,100 @@ return {
   },
   {
     "folke/snacks.nvim",
+    keys = {
+      {
+        "<leader><space>",
+        function()
+          resume_picker("files")
+        end,
+        desc = "Find Files (Root Dir)",
+      },
+      {
+        "<leader>/",
+        function()
+          resume_picker("grep")
+        end,
+        desc = "Grep (Root Dir)",
+      },
+      {
+        "<leader>ff",
+        function()
+          resume_picker("files")
+        end,
+        desc = "Find Files (Root Dir)",
+      },
+      {
+        "<leader>fF",
+        function()
+          resume_picker("files", { root = false })
+        end,
+        desc = "Find Files (cwd)",
+      },
+      {
+        "<leader>sg",
+        function()
+          resume_picker("grep")
+        end,
+        desc = "Grep (Root Dir)",
+      },
+      {
+        "<leader>sG",
+        function()
+          resume_picker("grep", { root = false })
+        end,
+        desc = "Grep (cwd)",
+      },
+    },
     opts = {
       dashboard = { enabled = false },
       picker = {
         layout = {
           preset = "vertical",
+        },
+        sources = {
+          explorer = {
+            actions = {
+              explorer_add = function(picker)
+                local ExplorerActions = require("snacks.explorer.actions")
+                local Tree = require("snacks.explorer.tree")
+                local uv = vim.uv or vim.loop
+
+                Snacks.input({
+                  prompt = 'Add a new file or directory (directories end with a "/")',
+                }, function(value)
+                  if not value or value:find("^%s*$") then
+                    return
+                  end
+
+                  local dir = picker:dir()
+                  local paths = vim.tbl_map(function(path)
+                    return {
+                      path = svim.fs.normalize(path),
+                      is_file = path:sub(-1) ~= "/",
+                    }
+                  end, vim.split(vim.fn.expand(dir .. "/" .. value), "\n", { trimempty = true }))
+
+                  for _, item in ipairs(paths) do
+                    if item.is_file and uv.fs_stat(item.path) then
+                      Snacks.notify.warn("File already exists:\n- `" .. item.path .. "`")
+                      return
+                    end
+                  end
+
+                  for _, item in ipairs(paths) do
+                    local target_dir = item.is_file and vim.fs.dirname(item.path) or item.path
+                    vim.fn.mkdir(target_dir, "p")
+                    if item.is_file then
+                      io.open(item.path, "w"):close()
+                    end
+                  end
+
+                  Tree:refresh(dir)
+                  ExplorerActions.update(picker, { target = paths[1].path, refresh = true })
+                end)
+              end,
+            },
+          },
         },
       },
     },
